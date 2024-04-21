c0a643facb2f7786b336f3507e0ee259f862f87e
Abhilash B K <bkabhilash0@gmail.com>
2024-04-21 18:35:42 +0530

    Added the repository rest resource annotation

--- src/main/java/com/spring/rest/api/course_basic/dao/EmployeeRepository.java
+++ src/main/java/com/spring/rest/api/course_basic/dao/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.spring.rest.api.course_basic.dao;
 
 import com.spring.rest.api.course_basic.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
